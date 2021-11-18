1d855baa612adc19fd89d94eb51a16078747e577
JoaoVirginio <52512363+JoaoVirginio@users.noreply.github.com>
2021-11-17 22:08:34 -0300

    deploy

--- app/models/db.js
+++ app/models/db.js
@@ -1,5 +1,9 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('diamond_db', 'root', '12345', {dialect: 'mysql', host: 'localhost', port: 3306});
+const sequelize = new Sequelize('d6f0m2b9p6aiud',
+ 'whebdjksvbzpgs',
+  '802c89e926fd6817e4736f9151091757c30f3897b13bff4792872ca5ddb90c1e',
+   {dialect: 'postgres', host: 'ec2-34-198-189-252.compute-1.amazonaws.com', port: 5432
+});
  
 module.exports = {
     Sequelize: Sequelize,
