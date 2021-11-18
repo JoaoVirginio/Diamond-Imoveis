de7cc007a13a38d802de86215606df88b13b8c65
JoaoVirginio <52512363+JoaoVirginio@users.noreply.github.com>
2021-11-17 21:09:39 -0300

    deploy

--- models/db.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('diamond_db', 'root', '12345', {dialect: 'mysql', host: 'localhost', port: 3306});
- 
-module.exports = {
-    Sequelize: Sequelize,
-    sequelize: sequelize
-} 
--- routes/index.js
+++
@@ -1,6 +0,0 @@
-const api = require('./api');
-const routes = require('express').Router();
-
-routes.use('/api', api);
-
-module.exports = routes;
