ac449e491c9e0e4144206923c57307bf05f1c9ff
JoaoVirginio <52512363+JoaoVirginio@users.noreply.github.com>
2021-11-17 21:43:37 -0300

    deploy

--- app/routes/property.js
+++ app/routes/property.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router()
-const { isClient } = require('../helpers/isclient');
+const { isClient } = require('../helpers/isclientc');
 
 const Property = require('../models/property')
 
